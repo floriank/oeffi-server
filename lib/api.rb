56a37259dd194274d7b6f32b2b25dc0bcdea5bb5
Florian Kraft <f.kraft@ewerk.com>
2013-10-28 17:52:27 +0100

    rename route

--- lib/api.rb
+++ lib/api.rb
@@ -1,5 +1,1 @@
-Dir["lib/vendor/*.jar"].each do |file|
-  require File.expand_path file
-end
-
 require File.expand_path "lib/api/stations"
--- lib/api/stations.rb
+++ lib/api/stations.rb
@@ -1,26 +1,25 @@
-Dir.glob File.expand_path("lib/oeffi/*.rb") do |file|
-  require file
-end
-
 module API
   class Stations < Grape::API
     params do
       requires :name, type: String, desc: "Name of a possible station/address/etc."
     end
-    post "/search" do
+    post "/suggest" do
       status 200
-      ::Oeffi::autocomplete(params.name)
+      Oeffi::autocomplete(params.name)
     end
 
     params do
       requires :from, desc: "Station for from"
       requires :to,  desc: "Station  for to"
       optional :via, desc: "Optional Via Station"
-      optional :connections,  desc: "Optional number of connections"
+      optional :count,  desc: "Optional number of connections"
+      optional :date,  desc: "Optional date to be used as departure/arrival time"
+      optional :departure,  desc: "Optional departure flag, if set to false, the date given is considered arrival time"
     end
-
     post "/trips" do
-      TripList.new(::Oeffi::query(params.from, params.to, params.via, params.connections)).as_json
+      status 200
+      trips = Oeffi::find_trips(params.from, params.to, params.via, params.count)
+      binding.pry
     end
   end
 end
